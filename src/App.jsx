41d93c64116d1fa779b70b4a2aef7be89a91d972
Arjun-1104 <Arjunrathore1104@gmail.com>
2024-11-06 21:02:46 +0530

    add items on enter click

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,43 @@ import ErrorMessages from './component/ErrorMessages';
 import Container from './component/Container';
 import FoodInput from './component/FoodInput';
 import './App.css';
+import { useState } from 'react';
 
 
 function App() {
-  let foodItems = ['Dal','Rice','Salad','Milk','Roti'];
+  // let foodItems = ['Dal','Rice','Salad','Milk','Roti'];
   // let foodItems = [];
+
+  let [textToShow,setTextToShow] = useState('Food Item Enter By User');
+  let [foodItems,setFoodItems] = useState([]);
+
+  let onKeyDown = (event) => {
+    if(event.key === 'Enter'){
+      let newItem = event.target.value;
+      event.target.value = '';
+      let newArr = [newItem,...foodItems];
+      setFoodItems(newArr);
+
+      
+    }
+    
+    // console.log(event.target.value);
+    // setTextToShow(event.target.value);
+  }
+
   return (
     <>
     <Container>
       <div className='w-100'>
         <AppName></AppName>
+        <FoodInput handleKeyDown={(event)=> onKeyDown(event)}></FoodInput>
         <ErrorMessages foodItem={foodItems}></ErrorMessages>
-        <FoodInput foodOnChange={(event)=>{console.log(event.target.value)}}></FoodInput>
+        {/* <p>{textToShow}</p> */}
         <FoodItems foodItem={foodItems}></FoodItems>
       </div>
     </Container>
     
-    <Container><p>This is a healthy food list.</p></Container>
+    {/* <Container><p>This is a healthy food list.</p></Container> */}
     </>
   )
 }
--- src/component/FoodInput.jsx
+++ src/component/FoodInput.jsx
@@ -1,12 +1,12 @@
 import style from '../styles/FoodInput.module.css';
 
 
-let FoodInput = ({foodOnChange}) => {
+let FoodInput = ({handleKeyDown}) => {
     // const foodOnChange = (event) => {
     //     console.log(event.target.value);
     // }
 
-    return <input type="text" className={`${style.foodInput}`} placeholder='Enter any food ?' onChange={foodOnChange}/>
+    return <input type="text" className={`${style.foodInput}`} placeholder='Enter any food ?' onKeyDown={handleKeyDown}/>
 }
 
 export default FoodInput;
